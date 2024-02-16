e98be2371d5dd130b810af67c3f1cbbd8c1a0cb5
SamanSah <sahraei.saman91@gmail.com>
2024-02-17 00:18:47 +0330

    init

---
+++ components/Parallax.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <img src="../assets/images/background.jpg" class="parallax bg-img" />
+        <img src="../assets/images/fog_7.jpg" class="parallax fog-7" />
+        <img src="../assets/images/mountain_10.jpg" class="parallax mountain-10" />
+        <img src="../assets/images/fog_6.jpg" class="parallax fog-6" />
+        <img src="../assets/images/mountain_9.jpg" class="parallax mountain-9" />
+        <img src="../assets/images/mountain_8.jpg" class="parallax mountain-8" />
+        <img src="../assets/images/fog_5.jpg" class="parallax fog-5" />
+        <img src="../assets/images/mountain_7.jpg" class="parallax mountain-7" />
+        <div class="text parallax">
+            <h2>China</h2>
+            <h1>Zhangjiajie</h1>
+        </div>
+        <img src="../assets/images/mountain_6.jpg" class="parallax mountain-6" />
+        <img src="../assets/images/fog_4.jpg" class="parallax fog-4" />
+        <img src="../assets/images/mountain_5.jpg" class="parallax mountain-5" />
+        <img src="../assets/images/fog_3.jpg" class="parallax fog-3" />
+        <img src="../assets/images/mountain_4.jpg" class="parallax mountain-4" />
+        <img src="../assets/images/mountain_3.jpg" class="parallax mountain-3" />
+        <img src="../assets/images/fog_2.jpg" class="parallax fog-2" />
+        <img src="../assets/images/mountain_2.jpg" class="parallax mountain-2" />
+        <img src="../assets/images/mountain_1.jpg" class="parallax mountain-1" />
+        <img src="../assets/images/sun_rays.jpg" class="sun-rays" />
+        <img src="../assets/images/black_shadow.jpg" class="black-shadow" />
+        <img src="../assets/images/fog_1.jpg" class="parallax fog-1" />
+
+        
+    </div>
+</template>
+<script setup>
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,206 @@
+<template>
+    <div class="main-box">
+        <div class="main">
+        <img src="/img/background.png" class="parallax bg-img" />
+        <img src="/img/fog_7.png" class="parallax fog-7" />
+        <img src="/img/mountain_10.png" class="parallax mountain-10" />
+        <img src="/img/fog_6.png" class="parallax fog-6" />
+        <img src="/img/mountain_9.png" class="parallax mountain-9" />
+        <img src="/img/mountain_8.png" class="parallax mountain-8" />
+        <img src="/img/fog_5.png" class="parallax fog-5" />
+        <img src="/img/mountain_7.png" class="parallax mountain-7" />
+        <div class="text parallax">
+            <h2>China</h2>
+            <h1>Zhangjiajie</h1>
+        </div>
+        <img src="/img/mountain_6.png" class="parallax mountain-6" />
+        <img src="/img/fog_4.png" class="parallax fog-4" />
+        <img src="/img/mountain_5.png" class="parallax mountain-5" />
+        <img src="/img/fog_3.png" class="parallax fog-3" />
+        <img src="/img/mountain_4.png" class="parallax mountain-4" />
+        <img src="/img/mountain_3.png" class="parallax mountain-3" />
+        <img src="/img/fog_2.png" class="parallax fog-2" />
+        <img src="/img/mountain_2.png" class="parallax mountain-2" />
+        <img src="/img/mountain_1.png" class="parallax mountain-1" />
+        <img src="/img/sun_rays.png" class="sun-rays" />
+        <!-- <img src="/img/black_shadow.png" class="black-shadow" />
+        <img src="/img/fog_1.png" class="parallax fog-1" /> -->
+
+        
+    </div>
+    </div>
+    
+</template>
+<script setup>
+
+</script>
+
+<style lang="sass" scoped>
+.main-box
+    width: 100vw
+    height: 930px
+    overflow: hidden
+    margin-bottom: 20em
+.main
+    width: 100%
+    height: 100%
+    position: relative
+    overflow: hidden
+.bg-img
+    position: absolute
+    width: 140vw
+    top: calc(50% - 400px)
+    left: calc(50% )
+    transform: translate(-50%, -50%)
+    z-index: 1
+.fog-7
+    position: absolute
+    width: 70vw
+    top: calc(50% - 440px)
+    left: calc(50% - 450px)
+    transform: translate(-50%, -50%)
+    z-index: 2
+.mountain-10
+    position: absolute
+    width: 500px
+    top: calc(50% - 130px)
+    left: calc(50% + 150px)
+    transform: translate(-50%, -50%)
+    z-index: 3
+.fog-6
+    position: absolute
+    width: 100vw
+    top: calc(50% + 130px)
+    left: calc(50% + 300px)
+    transform: translate(-50%, -50%)
+    z-index: 4
+
+.mountain-9
+    position: absolute
+    width: 400px
+    top: calc(50% + 140px)
+    left: calc(50% - 700px)
+    transform: translate(-50%, -50%)
+    z-index: 5
+.mountain-8
+    position: absolute
+    width: 700px
+    top: calc(50% + 140px)
+    left: calc(50% - 230px)
+    transform: translate(-50%, -50%)
+    z-index: 6
+.fog-5
+    position: absolute
+    width: 500px
+    top: calc(50% - 130px)
+    left: calc(50% + 150px)
+    transform: translate(-50%, -50%)
+    z-index: 7
+.mountain-7
+    position: absolute
+    width: 700px
+    top: calc(50% + 60px)
+    left: calc(50% + 100px)
+    transform: translate(-50%, -50%)
+    z-index: 8
+.mountain-6
+    position: absolute
+    width: 400px
+    top: calc(50% + 20px)
+    left: calc(50% + 660px)
+    transform: translate(-50%, -50%)
+    z-index: 10
+.fog-4
+    position: absolute
+    width: 500px
+    top: calc(50% - 200px)
+    left: calc(50% + 250px)
+    transform: translate(-50%, -50%)
+    z-index: 11
+.mountain-5
+    position: absolute
+    width: 600px
+    top: calc(50% + 260px)
+    left: calc(50% + 70px)
+    transform: translate(-50%, -50%)
+    z-index: 12
+.fog-3
+    position: absolute
+    width: 500px
+    top: calc(50% + 200px)
+    left: calc(50% + 250px)
+    transform: translate(-50%, -50%)
+    z-index: 13
+.mountain-4
+    position: absolute
+    width: 800px
+    top: calc(50% + 200px)
+    left: calc(50% + 375px)
+    transform: translate(-50%, -50%)
+    z-index: 14
+.mountain-3
+    position: absolute
+    width: 400px
+    top: calc(50% + 105px)
+    left: calc(50% + 830px)
+    transform: translate(-50%, -50%)
+    z-index: 15
+.fog-2
+    position: absolute
+    width: 500px
+    top: calc(50% - 200px)
+    left: calc(50% + 250px)
+    transform: translate(-50%, -50%)
+    z-index: 16
+.mountain-2
+    position: absolute
+    width: 600px
+    top: calc(50% + 190px)
+    left: calc(50% - 550px)
+    transform: translate(-50%, -50%)
+    z-index: 17
+.mountain-1
+    position: absolute
+    width: 400px
+    top: calc(50% + 135px)
+    left: calc(50% - 835px)
+    transform: translate(-50%, -50%)
+    z-index: 18
+.sun-rays
+    position: absolute
+    width: 500px
+    top: calc(50% - 450px)
+    left: calc(50% + 420px)
+    transform: translate(-50%, -50%)
+    z-index: 19
+.black-shadow
+    position: absolute
+    width: 100vw
+    top: calc(50% - 60px)
+    left: calc(50% )
+    transform: translate(-50%, -50%)
+    z-index: 20
+.fog-1
+    position: absolute
+    width: 100vw
+    top: calc(50% - 60px)
+    left: calc(50% )
+    transform: translate(-50%, -50%)
+    z-index: 21
+
+.text
+    position: absolute
+    z-index: 9
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    text-align: center
+    text-transform: uppercase
+    color: #ffffff
+    h2
+        font-weight: 100
+        font-size: 3rem
+    h1
+        font-size: 7rem    
+    
+</style>
