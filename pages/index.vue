233634a771cbfd02b0f2b6474edcc17fb5b56a0b
ofood-saman <ofood.saman@gmail.com>
2024-02-18 16:40:06 +0330

    update parallax

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,31 @@
 <template>
     <div class="main-box">
+        {{ xValue }} - {{ yValue }}
         <div class="main">
-        <img src="/img/background.png" class="parallax bg-img" />
-        <img src="/img/fog_7.png" class="parallax fog-7" />
-        <img src="/img/mountain_10.png" class="parallax mountain-10" />
-        <img src="/img/fog_6.png" class="parallax fog-6" />
-        <img src="/img/mountain_9.png" class="parallax mountain-9" />
-        <img src="/img/mountain_8.png" class="parallax mountain-8" />
-        <img src="/img/fog_5.png" class="parallax fog-5" />
-        <img src="/img/mountain_7.png" class="parallax mountain-7" />
-        <div class="text parallax">
-            <h2>China</h2>
-            <h1>Zhangjiajie</h1>
-        </div>
-        <img src="/img/mountain_6.png" class="parallax mountain-6" />
-        <img src="/img/fog_4.png" class="parallax fog-4" />
-        <img src="/img/mountain_5.png" class="parallax mountain-5" />
-        <img src="/img/fog_3.png" class="parallax fog-3" />
-        <img src="/img/mountain_4.png" class="parallax mountain-4" />
-        <img src="/img/mountain_3.png" class="parallax mountain-3" />
-        <img src="/img/fog_2.png" class="parallax fog-2" />
-        <img src="/img/mountain_2.png" class="parallax mountain-2" />
-        <img src="/img/mountain_1.png" class="parallax mountain-1" />
-        <img src="/img/sun_rays.png" class="sun-rays" />
-        <!-- <img src="/img/black_shadow.png" class="black-shadow" />
-        <img src="/img/fog_1.png" class="parallax fog-1" /> -->
+            <img src="/img/background.png" data-speedx="0.3" class="parallax bg-img" :style="computedStyle" />
+            <img src="/img/fog_7.png" data-speedx="0.3" class="parallax fog-7" :style="computedStyle" />
+            <img src="/img/mountain_10.png" class="parallax mountain-10" :style="computedStyle" />
+            <img src="/img/fog_6.png" class="parallax fog-6" :style="computedStyle" />
+            <img src="/img/mountain_9.png" class="parallax mountain-9" :style="computedStyle" />
+            <img src="/img/mountain_8.png" class="parallax mountain-8" :style="computedStyle" />
+            <img src="/img/fog_5.png" class="parallax fog-5" :style="computedStyle"  />
+            <img src="/img/mountain_7.png" class="parallax mountain-7" :style="computedStyle" />
+            <div class="text parallax">
+                <h2>China</h2>
+                <h1>Zhangjiajie</h1>
+            </div>
+            <img src="/img/mountain_6.png" class="parallax mountain-6" :style="computedStyle" />
+            <img src="/img/fog_4.png" class="parallax fog-4" />
+            <img src="/img/mountain_5.png" class="parallax mountain-5" :style="computedStyle" />
+            <img src="/img/fog_3.png" class="parallax fog-3" />
+            <img src="/img/mountain_4.png" class="parallax mountain-4" :style="computedStyle" />
+            <img src="/img/mountain_3.png" class="parallax mountain-3" :style="computedStyle" />
+            <img src="/img/fog_2.png" class="parallax fog-2" />
+            <img src="/img/mountain_2.png" class="parallax mountain-2" :style="computedStyle" />
+            <img src="/img/mountain_1.png" class="parallax mountain-1" :style="computedStyle" />
+            <img src="/img/sun_rays.png" class="sun-rays" />
+            <!-- <img src="/img/black_shadow.png" class="black-shadow" />
+            <img src="/img/fog_1.png" class="parallax fog-1" /> -->
 
         
     </div>
@@ -32,7 +33,36 @@
     
 </template>
 <script setup>
+import {ref, onMounted, reactive} from 'vue'
+import { useMouse, useWindowSize } from '@vueuse/core'
+const { width, height } = useWindowSize()
+const { x, y } = useMouse()
+const xValue = ref(null)
+const yValue = ref(null)
 
+const selectedElements = ref([])
+
+    
+onMounted(() => {
+    selectedElements.value = document.querySelectorAll('.parallax'); 
+    //   elements.value = Array.from(selectedElements);
+      console.log(selectedElements.value)
+  
+})
+
+watch(() => [x.value, y.value] ,([newX, newY])=> {
+    xValue.value = x.value - (width.value / 2)
+    yValue.value = y.value - (height.value / 2)
+    selectedElements.value.forEach(element => {
+        console.log(element.dataset.speedx);
+    })
+})
+
+const computedStyle = computed(() => {
+    return {
+        transform: `translateX(calc(-50% - ${xValue.value}px)) translateY(calc(-50% - ${yValue.value}px))`
+    }
+}) 
 </script>
 
 <style lang="sass" scoped>
@@ -51,28 +81,24 @@
     width: 140vw
     top: calc(50% - 400px)
     left: calc(50% )
-    transform: translate(-50%, -50%)
     z-index: 1
 .fog-7
     position: absolute
     width: 70vw
     top: calc(50% - 440px)
     left: calc(50% - 450px)
-    transform: translate(-50%, -50%)
     z-index: 2
 .mountain-10
     position: absolute
     width: 500px
     top: calc(50% - 130px)
     left: calc(50% + 150px)
-    transform: translate(-50%, -50%)
     z-index: 3
 .fog-6
     position: absolute
     width: 100vw
     top: calc(50% + 130px)
     left: calc(50% + 300px)
-    transform: translate(-50%, -50%)
     z-index: 4
 
 .mountain-9
@@ -80,77 +106,66 @@
     width: 400px
     top: calc(50% + 140px)
     left: calc(50% - 700px)
-    transform: translate(-50%, -50%)
     z-index: 5
 .mountain-8
     position: absolute
     width: 700px
     top: calc(50% + 140px)
     left: calc(50% - 230px)
-    transform: translate(-50%, -50%)
     z-index: 6
 .fog-5
     position: absolute
     width: 500px
     top: calc(50% - 130px)
     left: calc(50% + 150px)
-    transform: translate(-50%, -50%)
     z-index: 7
 .mountain-7
     position: absolute
     width: 700px
     top: calc(50% + 60px)
     left: calc(50% + 100px)
-    transform: translate(-50%, -50%)
     z-index: 8
 .mountain-6
     position: absolute
     width: 400px
     top: calc(50% + 20px)
     left: calc(50% + 660px)
-    transform: translate(-50%, -50%)
     z-index: 10
 .fog-4
     position: absolute
     width: 500px
     top: calc(50% - 200px)
     left: calc(50% + 250px)
-    transform: translate(-50%, -50%)
     z-index: 11
 .mountain-5
     position: absolute
     width: 600px
     top: calc(50% + 260px)
     left: calc(50% + 70px)
-    transform: translate(-50%, -50%)
     z-index: 12
 .fog-3
     position: absolute
     width: 500px
     top: calc(50% + 200px)
     left: calc(50% + 250px)
-    transform: translate(-50%, -50%)
     z-index: 13
 .mountain-4
     position: absolute
     width: 800px
     top: calc(50% + 200px)
     left: calc(50% + 375px)
-    transform: translate(-50%, -50%)
     z-index: 14
 .mountain-3
     position: absolute
     width: 400px
     top: calc(50% + 105px)
     left: calc(50% + 830px)
-    transform: translate(-50%, -50%)
     z-index: 15
 .fog-2
     position: absolute
     width: 500px
     top: calc(50% - 200px)
     left: calc(50% + 250px)
-    transform: translate(-50%, -50%)
     z-index: 16
 .mountain-2
     position: absolute
