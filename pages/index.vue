09969354ed883ecd2985133776216ec17c49f3bb
SamanSah <sahraei.saman91@gmail.com>
2024-02-19 00:15:12 +0330

    update

--- pages/index.vue
+++ pages/index.vue
@@ -4,38 +4,38 @@
         <div class="main">
             <img 
                 src="/img/background.png" 
-                data-speedx="0.3" 
-                class="parallax bg-img" 
-                 
+                data-speedx="0.3"
+                data-speedy="0.38" 
+                class="parallax bg-img"                 
             />
             <img 
                 src="/img/fog_7.png" 
                 data-speedx="0.27" 
+                data-speedy="0.32"
                 class="parallax fog-7" 
-                
             />
-            <img src="/img/mountain_10.png" data-speedx="0.195" class="parallax mountain-10"  />
-            <img src="/img/fog_6.png" data-speedx="0.25" class="parallax fog-6"  />
-            <img src="/img/mountain_9.png" data-speedx="0.125" class="parallax mountain-9"  />
-            <img src="/img/mountain_8.png" data-speedx="0.1" class="parallax mountain-8"  />
-            <img src="/img/fog_5.png" data-speedx="0.16" class="parallax fog-5"   />
-            <img src="/img/mountain_7.png" data-speedx="0.1" class="parallax mountain-7"  />
-            <div class="text parallax" data-speedx="0.07">
+            <img src="/img/mountain_10.png" data-speedx="0.195" data-speedy="0.305" class="parallax mountain-10"  />
+            <img src="/img/fog_6.png"       data-speedx="0.25"  data-speedy="0.28" class="parallax fog-6"  />
+            <img src="/img/mountain_9.png"  data-speedx="0.125" data-speedy="0.155" class="parallax mountain-9"  />
+            <img src="/img/mountain_8.png"  data-speedx="0.1"   data-speedy="0.11" class="parallax mountain-8"  />
+            <img src="/img/fog_5.png"       data-speedx="0.16"  data-speedy="0.105" class="parallax fog-5" />
+            <img src="/img/mountain_7.png"  data-speedx="0.1"   data-speedy="0.1" class="parallax mountain-7"  />
+            <div class="text parallax"      data-speedx="0.07"  data-speedy="0.7">
                 <h2>China</h2>
                 <h1>Zhangjiajie</h1>
             </div>
-            <img src="/img/mountain_6.png" data-speedx="0.065" class="parallax mountain-6"  />
-            <img src="/img/fog_4.png" data-speedx="0.135" class="parallax fog-4" />
-            <img src="/img/mountain_5.png" data-speedx="0.08" class="parallax mountain-5"  />
-            <img src="/img/fog_3.png" data-speedx="0.11" class="parallax fog-3" />
-            <img src="/img/mountain_4.png" data-speedx="0.059" class="parallax mountain-4"  />
-            <img src="/img/mountain_3.png" data-speedx="0.04" class="parallax mountain-3"  />
-            <img src="/img/fog_2.png" data-speedx="0.15" class="parallax fog-2" />
-            <img src="/img/mountain_2.png" data-speedx="0.0235" class="parallax mountain-2"  />
-            <img src="/img/mountain_1.png" data-speedx="0.027" class="parallax mountain-1"  />
+            <img src="/img/mountain_6.png"  data-speedx="0.065" data-speedy="0.05" class="parallax mountain-6"  />
+            <img src="/img/fog_4.png"       data-speedx="0.135" data-speedy="0.0155" class="parallax fog-4" />
+            <img src="/img/mountain_5.png"  data-speedx="0.08"  data-speedy="0.065" class="parallax mountain-5"  />
+            <img src="/img/fog_3.png"       data-speedx="0.11"  data-speedy="0.018" class="parallax fog-3" />
+            <img src="/img/mountain_4.png"  data-speedx="0.059" data-speedy="0.024" class="parallax mountain-4"  />
+            <img src="/img/mountain_3.png"  data-speedx="0.04"  data-speedy="0.018" class="parallax mountain-3"  />
+            <img src="/img/fog_2.png"       data-speedx="0.15"  data-speedy="0.0115" class="parallax fog-2" />
+            <img src="/img/mountain_2.png"  data-speedx="0.023" data-speedy="0.013" class="parallax mountain-2"  />
+            <img src="/img/mountain_1.png"  data-speedx="0.027" data-speedy="0.018" class="parallax mountain-1"  />
             <img src="/img/sun_rays.png" class="sun-rays" />
             <!-- <img src="/img/black_shadow.png" class="black-shadow" />
-            <img src="/img/fog_1.png" data-speedx="0.12" class="parallax fog-1" /> -->
+            <img src="/img/fog_1.png" data-speedx="0.12" data-speedy="0.01" class="parallax fog-1"  /> -->
 
         
     </div>
@@ -51,15 +51,19 @@ const xValue = ref(null)
 const yValue = ref(null)
 
 const selectedElements = ref([])
-const computedStyle = computed(() => {
-    return {
-        transform: `translateX(calc(-50% - ${xValue.value}px)) translateY(calc(-50% - ${yValue.value}px))`
-    }
-}) 
+// const computedStyle = computed( {
+//         get() {
+//             return {
+//                 transform: `translateX(calc(-50% - ${xValue.value}px)) translateY(calc(-50% - ${yValue.value}px))`
+//             }
+//         },
+//         set(newValue) {
+//             console.log('newValue', newValue)
+//         }  
+//     }) 
     
 onMounted(() => {
-    selectedElements.value = document.querySelectorAll('.parallax'); 
-    //   elements.value = Array.from(selectedElements);
+    selectedElements.value = document.querySelectorAll('.parallax')
       console.log(selectedElements.value)
   
 })
@@ -68,9 +72,20 @@ watch(() => [x.value, y.value] ,([newX, newY])=> {
     xValue.value = x.value - (width.value / 2)
     yValue.value = y.value - (height.value / 2)
     selectedElements.value.forEach(element => {
-        console.log(element.dataset.speedx);
-        element.style.transform = `translateX(calc(-50% - ${xValue.value}px)) translateY(calc(-50% - ${yValue.value}px))`
+       let speedx = element.dataset.speedx
+       let speedy = element.dataset.speedy
+        element.style.transform = `translateX(calc(-50% - ${xValue.value * speedx}px)) translateY(calc(-50% - ${yValue.value * speedy}px))`
     })
+    // computedStyle.value = computed({
+    //     get() {
+    //         return {
+    //             transform: `translateX(calc(-50% - ${xValue.value * }px)) translateY(calc(-50% - ${yValue.value}px))`
+    //         }
+    //     },
+    //     set(newValue) {
+    //         transform: newValue
+    //     }  
+    // }) 
 })
 
 
@@ -87,6 +102,10 @@ watch(() => [x.value, y.value] ,([newX, newY])=> {
     height: 100%
     position: relative
     overflow: hidden
+
+.parallax
+    pointer-events: none
+    // transition: 0.45s cubic-bezier(.2, .49, .32, .99)
 .bg-img
     position: absolute
     width: 140vw
