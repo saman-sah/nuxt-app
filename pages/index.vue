eb7102b9e4959794fac16e3aec970e3072294c50
SamanSah <sahraei.saman91@gmail.com>
2024-02-19 18:35:22 +0330

    update animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,42 @@
 <template>
     <div class="main-box">
-        {{ xValue }} - {{ yValue }}
         <div class="main">
             <img 
                 src="/img/background.png" 
                 data-speedx="0.3"
-                data-speedy="0.38" 
+                data-speedy="0.28"
+                data-speedz="0"
                 class="parallax bg-img"                 
             />
             <img 
                 src="/img/fog_7.png" 
                 data-speedx="0.27" 
                 data-speedy="0.32"
+                data-speedz="0"
                 class="parallax fog-7" 
             />
-            <img src="/img/mountain_10.png" data-speedx="0.195" data-speedy="0.305" class="parallax mountain-10"  />
-            <img src="/img/fog_6.png"       data-speedx="0.25"  data-speedy="0.28" class="parallax fog-6"  />
-            <img src="/img/mountain_9.png"  data-speedx="0.125" data-speedy="0.155" class="parallax mountain-9"  />
-            <img src="/img/mountain_8.png"  data-speedx="0.1"   data-speedy="0.11" class="parallax mountain-8"  />
-            <img src="/img/fog_5.png"       data-speedx="0.16"  data-speedy="0.105" class="parallax fog-5" />
-            <img src="/img/mountain_7.png"  data-speedx="0.1"   data-speedy="0.1" class="parallax mountain-7"  />
-            <div class="text parallax"      data-speedx="0.07"  data-speedy="0.7">
+            <img src="/img/mountain_10.png" data-speedx="0.195" data-speedy="0.305" data-speedz="0" class="parallax mountain-10"  />
+            <img src="/img/fog_6.png"       data-speedx="0.25"  data-speedy="0.28"  data-speedz="0" class="parallax fog-6"  />
+            <img src="/img/mountain_9.png"  data-speedx="0.15" data-speedy="0.155"  data-speedz="0" class="parallax mountain-9"  />
+            <img src="/img/mountain_8.png"  data-speedx="0.1"   data-speedy="0.11"  data-speedz="0" class="parallax mountain-8"  />
+            <img src="/img/fog_5.png"       data-speedx="0.16"  data-speedy="0.105" data-speedz="0" class="parallax fog-5" />
+            <img src="/img/mountain_7.png"  data-speedx="0.1"   data-speedy="0.1"   data-speedz="0" class="parallax mountain-7"  />
+            <div class="text parallax"      data-speedx="0.07"  data-speedy="0.2"   data-speedz="0">
                 <h2>China</h2>
                 <h1>Zhangjiajie</h1>
             </div>
-            <img src="/img/mountain_6.png"  data-speedx="0.065" data-speedy="0.05" class="parallax mountain-6"  />
-            <img src="/img/fog_4.png"       data-speedx="0.135" data-speedy="0.0155" class="parallax fog-4" />
-            <img src="/img/mountain_5.png"  data-speedx="0.08"  data-speedy="0.065" class="parallax mountain-5"  />
-            <img src="/img/fog_3.png"       data-speedx="0.11"  data-speedy="0.018" class="parallax fog-3" />
-            <img src="/img/mountain_4.png"  data-speedx="0.059" data-speedy="0.024" class="parallax mountain-4"  />
-            <img src="/img/mountain_3.png"  data-speedx="0.04"  data-speedy="0.018" class="parallax mountain-3"  />
-            <img src="/img/fog_2.png"       data-speedx="0.15"  data-speedy="0.0115" class="parallax fog-2" />
-            <img src="/img/mountain_2.png"  data-speedx="0.023" data-speedy="0.013" class="parallax mountain-2"  />
-            <img src="/img/mountain_1.png"  data-speedx="0.027" data-speedy="0.018" class="parallax mountain-1"  />
+            <img src="/img/mountain_6.png"  data-speedx="0.065" data-speedy="0.05"  data-speedz="0.1" class="parallax mountain-6"  />
+            <img src="/img/fog_4.png"       data-speedx="0.135" data-speedy="0.015" data-speedz="0.65" class="parallax fog-4" />
+            <img src="/img/mountain_5.png"  data-speedx="0.08"  data-speedy="0.065" data-speedz="0.43" class="parallax mountain-5"  />
+            <img src="/img/fog_3.png"       data-speedx="0.11"  data-speedy="0.018" data-speedz="0" class="parallax fog-3" />
+            <img src="/img/mountain_4.png"  data-speedx="0.059" data-speedy="0.024" data-speedz="0" class="parallax mountain-4"  />
+            <img src="/img/mountain_3.png"  data-speedx="0.04"  data-speedy="0.018" data-speedz="0.32" class="parallax mountain-3"  />
+            <img src="/img/fog_2.png"       data-speedx="0.15"  data-speedy="0.011" data-speedz="0" class="parallax fog-2" />
+            <img src="/img/mountain_2.png"  data-speedx="0.023" data-speedy="0.013" data-speedz="0.54" class="parallax mountain-2"  />
+            <img src="/img/mountain_1.png"  data-speedx="0.027" data-speedy="0.018" data-speedz="0.63" class="parallax mountain-1"  />
             <img src="/img/sun_rays.png" class="sun-rays" />
-            <!-- <img src="/img/black_shadow.png" class="black-shadow" />
-            <img src="/img/fog_1.png" data-speedx="0.12" data-speedy="0.01" class="parallax fog-1"  /> -->
+            <!-- <img src="/img/black_shadow.png" class="black-shadow" data-speedz="0" />
+            <img src="/img/fog_1.png" data-speedx="0.12" data-speedy="0.01" class="parallax fog-1" data-speedz="0" /> -->
 
         
     </div>
@@ -49,6 +50,7 @@ const { width, height } = useWindowSize()
 const { x, y } = useMouse()
 const xValue = ref(null)
 const yValue = ref(null)
+const zValue = ref(null)
 
 const selectedElements = ref([])
 // const computedStyle = computed( {
@@ -69,12 +71,22 @@ onMounted(() => {
 })
 
 watch(() => [x.value, y.value] ,([newX, newY])=> {
-    xValue.value = x.value - (width.value / 2)
-    yValue.value = y.value - (height.value / 2)
+   
+
     selectedElements.value.forEach(element => {
-       let speedx = element.dataset.speedx
-       let speedy = element.dataset.speedy
-        element.style.transform = `translateX(calc(-50% - ${xValue.value * speedx}px)) translateY(calc(-50% - ${yValue.value * speedy}px))`
+        let isInLeft = parseFloat(getComputedStyle(element).left) < width.value / 2 ? 1 : -1
+
+        xValue.value = x.value - (width.value / 2)
+        yValue.value = y.value - (height.value / 2)
+        zValue.value = (x.value - parseFloat(getComputedStyle(element).left)) * isInLeft * 0.3
+
+        let speedx = element.dataset.speedx
+        let speedy = element.dataset.speedy
+        let speedz = element.dataset.speedz
+
+        element.style.transform = `translateX(calc(-50% - ${xValue.value * speedx}px)) 
+        translateY(calc(-50% - ${yValue.value * speedy}px)) 
+        perspective(2300px) translateZ( ${zValue.value * speedz}px)`
     })
     // computedStyle.value = computed({
     //     get() {
@@ -105,7 +117,7 @@ watch(() => [x.value, y.value] ,([newX, newY])=> {
 
 .parallax
     pointer-events: none
-    // transition: 0.45s cubic-bezier(.2, .49, .32, .99)
+    transition: 0.45s cubic-bezier(.2, .49, .32, .99)
 .bg-img
     position: absolute
     width: 140vw
